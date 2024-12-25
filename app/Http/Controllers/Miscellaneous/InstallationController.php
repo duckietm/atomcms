55079d0e6446da3e32e3a48143334d8784087530
ObjectRetros <dennisobject@gmail.com>
2024-12-25 20:09:48 +0100

    feat: dynamically fetch web settings upon installation

--- app/Http/Controllers/Miscellaneous/InstallationController.php
+++ app/Http/Controllers/Miscellaneous/InstallationController.php
@@ -86,33 +86,23 @@ class InstallationController extends Controller
     {
         foreach ($request->except('_token') as $key => $value) {
             WebsiteSetting::where('key', '=', $key)->update([
-                'value' => $value
+                'value' => $value ?? '',
             ]);
         }
     }
 
     private function getSettingsForStep(int $step)
     {
+        $settingsData = array_chunk(WebsiteSetting::all()->pluck('key')->toArray(), ceil(WebsiteSetting::count() / 4));
+
         $settings = match ($step) {
-            1 => ['theme', 'hotel_name', 'rcon_ip', 'rcon_port', 'avatar_imager', 'discord_invitation_link', 'discord_widget_id'],
-            2 => ['start_motto', 'start_credits', 'start_duckets', 'start_diamonds', 'start_points', 'start_look', 'max_accounts_per_ip'],
-            3 => ['referrals_needed', 'referral_reward_amount', 'referral_reward_currency_type', 'min_staff_rank', 'maintenance_message', 'requires_beta_code', 'disable_registration', 'cms_color_mode'],
-            4 => [
-                'give_hc_on_register',
-                'hc_on_register_duration',
-                'max_comment_per_article',
-                'website_wordfilter_enabled',
-                'vpn_block_enabled',
-                'ipdata_api_key',
-                'housekeeping_url',
-                'force_staff_2fa',
-                'enable_discord_webhook',
-                'discord_webhook_url',
-            ],
+            1 => $settingsData[0] ?? [],
+            2 => $settingsData[1] ?? [],
+            3 => $settingsData[2] ?? [],
+            4 => $settingsData[3] ?? [],
             5 => [], // Completion step has no settings
             default => throw new \Exception('Step does not exist'),
         };
-
 
         return WebsiteSetting::query()
             ->whereIn('key', $settings)
