4ba007c89a1b3dc09e92aee6fb52de25e57a24c4
Dennis <dennisobject@gmail.com>
2024-12-29 21:40:09 +0100

    fix: username listing in audit logs hk table

--- app/Filament/Resources/Audit/UserAuditResource.php
+++ app/Filament/Resources/Audit/UserAuditResource.php
@@ -7,14 +7,16 @@ use App\Models\Audit\UserAudit;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\HtmlString;
 use Rmsramos\Activitylog\Resources\ActivitylogResource;
 
 class UserAuditResource extends ActivitylogResource
 {
-	
 	public static function table(Table $table): Table
     {
         return $table
@@ -23,7 +25,16 @@ class UserAuditResource extends ActivitylogResource
 					->toggleable(true),
                 static::getEventColumnCompoment(),
                 static::getSubjectTypeColumnCompoment(),
-                static::getCauserNameColumnCompoment(),
+                TextColumn::make('causer.name')
+                    ->label(__('activitylog::tables.columns.causer.label'))
+                    ->getStateUsing(function (Model $record) {
+                        if ($record->causer_id == null) {
+                            return new HtmlString('&mdash;');
+                        }
+
+                        return $record->causer->username;
+                    })
+                    ->searchable(),
                 static::getPropertiesColumnCompoment(),
                 static::getCreatedAtColumnCompoment(),
             ])
